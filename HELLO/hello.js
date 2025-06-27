52f0934749bfc7f6ea35c31f9909002d0224e9bd
ZAHIDMIANA <zahidmiana6715@gmail.com>
2025-06-27 11:22:37 +0500

    Initial commit: Added Express and HELLO projects

---
+++ HELLO/hello.js
@@ -0,0 +1,4 @@
+const math = require('./math');
+
+console.log("The Sum is: ", math.subFn(10,20));
+console.log("The Sum is: ", math.mulFn(10,20));
---
+++ HELLO/math.js
@@ -0,0 +1,18 @@
+function add(a, b){
+    return a+b;
+}
+
+function subtract(c, d){
+    return c-d;
+}
+
+function mul(h, i){
+    return h*i;
+}
+
+
+module.exports = {
+    addFn : add,
+    subFn: subtract,
+    mulFn: mul
+}
